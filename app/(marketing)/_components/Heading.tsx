e76f95095f6f56119d5f7d966f661c2d89705314
Osaze Oviawe <ocubed88@gmail.com>
2023-10-14 16:16:55 +0100

    feat: - added home page - added sign in and out functionality

--- app/(marketing)/_components/Heading.tsx
+++ app/(marketing)/_components/Heading.tsx
@@ -1,23 +1,47 @@
 "use client";
 
+import { useConvexAuth } from "convex/react";
+
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
+import { SignInButton } from "@clerk/clerk-react";
 
 export const Heading = () => {
+	const { isAuthenticated, isLoading } = useConvexAuth();
+
 	return (
-		<div className="max-w-3xl space-y-4">
+		<div className="max-w-4xl space-y-4">
 			<h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-				Your Ideas, Documents, & Plans. Unified. Welcome to{" "}
-				<span className="underline">NoteWave</span>
+				Your Concepts, Papers, and Strategies. One. Unified. Greetings
+				and welcome to <span className="underline">NoteWave</span>
 			</h1>
 			<h3 className="text-base sm:text-xl md:text-2xl">
-				NoteWave is the connected workspace where <br />
-				better, faster work happens.
+				The connected workspace where improved, more efficient work is
+				performed is NoteWave.
 			</h3>
-			<Button>
-				Enter NoteWave
-				<ArrowRight className="h-4 w-4 ml-2" />
-			</Button>
+			{isLoading && (
+				<div className="flex w-full items-center justify-center">
+					<Spinner size="lg" />
+				</div>
+			)}
+			{isAuthenticated && !isLoading && (
+				<Button asChild>
+					<Link href="/documents">
+						Enter NoteWave
+						<ArrowRight className="h-4 w-4 ml-2" />
+					</Link>
+				</Button>
+			)}
+			{!isAuthenticated && !isLoading && (
+				<SignInButton mode="modal">
+					<Button>
+						Get NoteWave free
+						<ArrowRight className="h-4 w-4 ml-2" />
+					</Button>
+				</SignInButton>
+			)}
 		</div>
 	);
 };

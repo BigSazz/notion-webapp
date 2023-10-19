7009644312f791760348c21979efe594f3290a5d
Osaze Oviawe <ocubed88@gmail.com>
2023-10-19 15:01:28 +0100

    - navbar component completed

--- app/(main)/_components/Navbar.tsx
+++ app/(main)/_components/Navbar.tsx
@@ -8,6 +8,9 @@ import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 
 import { Title } from "./Title";
+import { Banner } from "./Banner";
+import { Menu } from "./Menu";
+import { Publish } from "./Publish";
 // import { Banner } from "./banner";
 // import { Menu } from "./menu";
 // import { Publish } from "./publish";
@@ -28,9 +31,9 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
 		return (
 			<nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between">
 				<Title.Skeleton />
-				{/* <div className="flex items-center gap-x-2">
-          <Menu.Skeleton />
-        </div> */}
+				<div className="flex items-center gap-x-2">
+					<Menu.Skeleton />
+				</div>
 			</nav>
 		);
 	}
@@ -51,15 +54,15 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
 				)}
 				<div className="flex items-center justify-between w-full">
 					<Title initialData={document} />
-					{/* <div className="flex items-center gap-x-2">
-            <Publish initialData={document} />
-            <Menu documentId={document._id} />
-          </div> */}
+					<div className="flex items-center gap-x-2">
+						<Publish initialData={document} />
+						<Menu documentId={document._id} />
+					</div>
 				</div>
 			</nav>
-			{/* {document.isArchived && (
-        <Banner documentId={document._id} />
-      )} */}
+			{document.isArchived && (
+				<Banner documentId={document._id} isCollapsed={isCollapsed} />
+			)}
 		</>
 	);
 };

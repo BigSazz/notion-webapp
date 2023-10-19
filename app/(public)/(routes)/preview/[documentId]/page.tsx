3c10c91452615e0cd80aa7ad2fa20acfef2ef696
Osaze Oviawe <ocubed88@gmail.com>
2023-10-19 20:41:31 +0100

    - added publish page

--- app/(public)/(routes)/preview/[documentId]/page.tsx
+++ app/(public)/(routes)/preview/[documentId]/page.tsx
@@ -9,7 +9,6 @@ import { Id } from "@/convex/_generated/dataModel";
 import { Toolbar } from "@/components/toolbar";
 import { Cover } from "@/components/cover";
 import { Skeleton } from "@/components/ui/skeleton";
-// import Editor from "@/components/editor";
 
 interface DocumentIdPageProps {
 	params: {
@@ -57,18 +56,17 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
 	}
 
 	return (
-		<>
-			<div className="pb-40">
-				<Cover url={document.coverImage} />
-				<div className="md:max-w-3xl lg:max-w-4xl mx-auto">
-					<Toolbar initialData={document} />
-					<Editor
-						onChange={onChange}
-						initialContent={document.content}
-					/>
-				</div>
+		<div className="pb-40">
+			<Cover preview url={document.coverImage} />
+			<div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+				<Toolbar preview initialData={document} />
+				<Editor
+					editable={false}
+					onChange={onChange}
+					initialContent={document.content}
+				/>
 			</div>
-		</>
+		</div>
 	);
 };
 
